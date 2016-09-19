7b249717527d234cd351adf612363e5b36fdc2e5
Ling Tze Hock <thock_900805@hotmail.com>
2016-09-19 17:57:31 +0800

    add google interstitial

--- belugaSDK/src/main/java/com/hosengamers/beluga/ads/InterstitialAdListener.java
+++ belugaSDK/src/main/java/com/hosengamers/beluga/ads/InterstitialAdListener.java
@@ -4,4 +4,13 @@ package com.hosengamers.beluga.ads;
  * Created by user on 2016/9/19.
  */
 public interface InterstitialAdListener {
+    public abstract void onAdLoaded();
+
+    public abstract void onAdFailedToLoad(String paramString);
+
+    public abstract void onAdOpened();
+
+    public abstract void onAdClosed();
+
+    public abstract void onAdLeftApplication();
 }
